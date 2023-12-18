4e9a0a5ac8117486eb7cfa51f58846ecf884005d
Awet Tsegazeab <avvvet@gmail.com>
2023-12-18 06:21:55 -0500

    gitignore

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,9 +1,34 @@
 package utils
 
-import "os/exec"
+import (
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+// func ExecuteCommands(command string) (string, error) {
+// 	cmd := exec.Command("/bin/sh", "-c", command)
+
+// 	output, err := cmd.CombinedOutput()
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	return string(output), nil
+// }
 
 func ExecuteCommands(command string) (string, error) {
-	cmd := exec.Command("/bin/sh", "-c", command)
+	cmdParts := strings.Fields(command)
+	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
+
+	// Setup a pipe to pass the sudo password
+	password := "awet_awet"
+	cmd.Stdin = strings.NewReader(password + "\n")
+
+	// For handling interactive sudo password prompts
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
