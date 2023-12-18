dab4cc6081aa7bfbf9d47bb23c6fb212283fb86f
Awet Tsegazeab <avvvet@gmail.com>
2023-12-18 07:52:22 -0500

    update cmd and readme

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ## what is it ?
 
 Guardian-shell is a  secure reverse shell tool written in Go. Designed with a focus on responsible use and ethical practices, it facilitates secure communication between authorized parties. Guardian-shell empowers you with the ability to remotely execute commands on target machines, for easy system management and monitoring. 
+
+
+## usage
+
+to add new user in remote linux server
+```
+echo ur_sudo_password | sudo -S useradd testuser; echo new_password_123 | passwd testuser
+```
+
+to delete user from remote linux server
+```
+echo ur_sudo_password | sudo -S userdel testuser
+```
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -3,32 +3,10 @@ package utils
 import (
 	"os/exec"
 	"strings"
-	"syscall"
 )
 
-// func ExecuteCommands(command string) (string, error) {
-// 	cmd := exec.Command("/bin/sh", "-c", command)
-
-// 	output, err := cmd.CombinedOutput()
-// 	if err != nil {
-// 		return "", err
-// 	}
-
-// 	return string(output), nil
-// }
-
 func ExecuteCommands(command string) (string, error) {
-	cmdParts := strings.Fields(command)
-	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
-
-	// Setup a pipe to pass the sudo password
-	password := "awet_awet"
-	cmd.Stdin = strings.NewReader(password + "\n")
-
-	// For handling interactive sudo password prompts
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true,
-	}
+	cmd := exec.Command("/bin/sh", "-c", command)
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
@@ -36,4 +14,46 @@ func ExecuteCommands(command string) (string, error) {
 	}
 
 	return string(output), nil
+}
+
+// func ExecuteCommands(command string) (string, error) {
+// 	// Check if the command is interactive
+// 	interactive := isInteractive(command)
+
+// 	// Split the command into parts
+// 	cmdParts := strings.Fields(command)
+
+// 	// Use sudo to execute the command if interactive
+// 	if interactive {
+// 		cmd := exec.Command("sudo", append([]string{"-S"}, cmdParts...)...)
+// 		// Setup a pipe to pass the sudo password
+// 		password := "your_password"
+// 		cmd.Stdin = strings.NewReader(password + "\n")
+
+// 		// Run the command
+// 		output, err := cmd.CombinedOutput()
+// 		if err != nil {
+// 			return "", err
+// 		}
+// 		return string(output), nil
+// 	}
+
+// 	// For non-interactive commands, run them directly
+// 	cmd := exec.Command(cmdParts[0], cmdParts[1:]...)
+// 	output, err := cmd.CombinedOutput()
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	return string(output), nil
+// }
+
+func isInteractive(command string) bool {
+	// Check for keywords or characters that may suggest interactivity
+	interactiveKeywords := []string{"sudo", "su", "passwd", "ssh", "psql", "mysql", "bash"}
+	for _, keyword := range interactiveKeywords {
+		if strings.Contains(command, keyword) {
+			return true
+		}
+	}
+	return false
 }
